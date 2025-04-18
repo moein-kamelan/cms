68261a6d75425cd2ba7dd42c409d500e723e5412
moein801 <moeinkamelan@gmail.com>
2025-04-18 23:25:35 +0430

    final commit in home on 2025/4/18

--- src/app/create-new-user/create-new-user.component.ts
+++ src/app/create-new-user/create-new-user.component.ts
@@ -1,12 +1,72 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from "../shared/header/header.component";
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from '../shared/header/header.component';
+import { MaterialModule } from '../material.module';
+import { FormControl, FormControlName, FormGroup, Validators } from '@angular/forms';
+import { FormFeildComponent } from "../shared/form-feild/form-feild.component";
+import { ErrorMessageComponent } from "../shared/error-message/error-message.component";
 
 @Component({
   selector: 'app-create-new-user',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, MaterialModule, FormFeildComponent, ErrorMessageComponent],
   templateUrl: './create-new-user.component.html',
-  styleUrl: './create-new-user.component.css'
+  styleUrl: './create-new-user.component.css',
 })
-export class CreateNewUserComponent {
+export class CreateNewUserComponent implements OnInit {
+  createNewUserFormGroup!: FormGroup;
 
+
+  ngOnInit(): void {
+    this.createNewUserFormGroup = new FormGroup({
+      firstName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(12),
+      ]),
+      lastName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(12),
+      ]),
+      fatherName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(12),
+      ]),
+      userName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(12),
+      ]),
+      mobileNumber: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^09\d{9}$/),
+      ]),
+      organizationId: new FormControl(0, []),
+      nationalCode: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^(?!(\d)\1{9})\d{10}$/),
+      ]),
+      identifyNumber: new FormControl('', []),
+      isForeigner: new FormControl(false, []),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(12),
+        Validators.pattern(
+          /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/
+        ),
+      ]),
+    });
+  }
+
+  onSubmitCreateNewUserForm() {
+    console.log("hello");
+    
+  }
+  
+  onclickSubmitButton() {
+    console.log(this.createNewUserFormGroup.valid);
+    
+  }
+  
 }
--- src/app/edit-new-user/edit-new-user.component.ts
+++ src/app/edit-new-user/edit-new-user.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from "../shared/header/header.component";
 import { ActivatedRoute } from '@angular/router';
+import { ErrorMessageComponent } from "../shared/error-message/error-message.component";
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MaterialModule } from '../material.module';
 
 @Component({
   selector: 'app-edit-new-user',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ErrorMessageComponent , MaterialModule],
   templateUrl: './edit-new-user.component.html',
   styleUrl: './edit-new-user.component.css'
 })
 export class EditNewUserComponent implements OnInit {
 
   userID : string | null = null
+  editUserFormGroup! : FormGroup
   constructor(private route : ActivatedRoute) {
     
   }
@@ -18,6 +22,48 @@ export class EditNewUserComponent implements OnInit {
   ngOnInit(): void {
       this.userID = this.route.snapshot.paramMap.get("id")
       console.log('this.userID:', this.userID)
+      this.editUserFormGroup = new FormGroup({
+        firstName: new FormControl('', [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(12),
+        ]),
+        lastName: new FormControl('', [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(12),
+        ]),
+        fatherName: new FormControl('', [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(12),
+        ]),
+        userName: new FormControl('', [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(12),
+        ]),
+        mobileNumber: new FormControl('', [
+          Validators.required,
+          Validators.pattern(/^09\d{9}$/),
+        ]),
+        organizationId: new FormControl(0, []),
+        nationalCode: new FormControl('', [
+          Validators.required,
+          Validators.pattern(/^(?!(\d)\1{9})\d{10}$/),
+        ]),
+        identifyNumber: new FormControl('', []),
+        isForeigner: new FormControl(false, []),
+        
+      });
+
+  }
+
+  onclickSubmitButton() {
+
+  }
+
+  onSubmitEditUserForm() {
 
   }
 
